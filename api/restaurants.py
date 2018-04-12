8826adf667e96d1af555e89e7f18ed38253fc42c
Navendra Jha <navendra@asu.edu>
2018-04-12 16:14:02 -0700

    Modified restaurants model to inject restaurant interface as per new injector specifications

--- api/restaurants.py
+++ api/restaurants.py
@@ -1,6 +1,14 @@
 from flask_injector import inject
 from services.provider import RestaurantsProvider
 
-@inject(data_provider = RestaurantsProvider)
-def search(data_provider) -> list:
+
+#Connexion's RestyResolver looks for search() function for get operations
+@inject
+def search(data_provider : RestaurantsProvider.RestaurantsProvider) -> list:
     return data_provider.get()
+
+
+#Connexion's RestyResolver looks for post() function for post operations
+@inject
+def post(data_provider : RestaurantsProvider.RestaurantsProvider):
+    x=3
